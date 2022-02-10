1ef45f8dc885286f77d320f761874082822acd08
Wilmer Diaz <wdiazplus@Wilmers-Mac-mini.local>
2022-02-10 17:43:03 -0500

    Add Daily Coding 1

---
+++ easy/high_order_functions_and_list_comprehensions/main.py
@@ -0,0 +1,89 @@
+DATA = [
+    {
+        'name': 'Facundo',
+        'age': 72,
+        'organization': 'Platzi',
+        'position': 'Technical Coach',
+        'language': 'python',
+    },
+    {
+        'name': 'Luisana',
+        'age': 33,
+        'organization': 'Globant',
+        'position': 'UX Designer',
+        'language': 'javascript',
+    },
+    {
+        'name': 'Héctor',
+        'age': 19,
+        'organization': 'Platzi',
+        'position': 'Associate',
+        'language': 'ruby',
+    },
+    {
+        'name': 'Gabriel',
+        'age': 20,
+        'organization': 'Platzi',
+        'position': 'Associate',
+        'language': 'javascript',
+    },
+    {
+        'name': 'Isabella',
+        'age': 30,
+        'organization': 'Platzi',
+        'position': 'QA Manager',
+        'language': 'java',
+    },
+    {
+        'name': 'Karo',
+        'age': 23,
+        'organization': 'Everis',
+        'position': 'Backend Developer',
+        'language': 'python',
+    },
+    {
+        'name': 'Ariel',
+        'age': 32,
+        'organization': 'Rappi',
+        'position': 'Support',
+        'language': '',
+    },
+    {
+        'name': 'Juan',
+        'age': 17,
+        'organization': '',
+        'position': 'Student',
+        'language': 'go',
+    },
+    {
+        'name': 'Pablo',
+        'age': 32,
+        'organization': 'Master',
+        'position': 'Human Resources Manager',
+        'language': 'python',
+    },
+    {
+        'name': 'Lorena',
+        'age': 56,
+        'organization': 'Python Organization',
+        'position': 'Language Maker',
+        'language': 'python',
+    },
+]
+
+def run():
+    all_python_devs = list(filter(lambda employer: employer['language'] == 'python',DATA))
+    print("------------------")
+    print(all_python_devs)
+    all_platzi_workers = list(filter(lambda worker : worker['organization'] == 'Platzi',DATA))
+    print("------------------")
+    print(all_platzi_workers)
+    old_people= [ people['name'] for people in DATA if people['age'] >= 60 ]
+    print("------------------")
+    print(old_people)
+    
+
+
+
+if __name__ == '__main__':
+    run()
